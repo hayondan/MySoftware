2577704502f1c097d5135901117832865b5cfb53
hayondan <hayondan@gmail.com>
2021-02-24 16:37:52 +0200

    commit

---
+++ jankinsfile.py
@@ -0,0 +1,25 @@
+
+pipeline {
+    agent any
+    stages {
+        stage('checkout') {
+            steps {
+                script {
+                    properties([pipelineTriggers([pollSCM('* * * * *')])])
+                }
+                git 'https://github.com/hayondan/MySoftware.git'
+            }
+        }
+        stage('run python') {
+            steps {
+                script {
+                    if (Boolean.valueOf(env.UNIX)) {
+                        sh 'change.py'
+                    } else {
+                        bat 'change.py'
+                    }
+                }
+            }
+        }
+    }
+}
